5400274992574992856eb3a3c1c6d475be5b162b
philipjc <philipcox83@gmail.com>
2017-01-17 11:38:14 +0000

    started.

--- 02 - JS + CSS Clock/clock.js
+++ 02 - JS + CSS Clock/clock.js
@@ -1,6 +1,29 @@
 
-class clock {
+class Clock {
 
+  constructor() {
+    this.hourHand = document.querySelector('.hour-hand');
+    this.minHand = document.querySelector('.min-hand');
+    this.secondHand = document.querySelector('.second-hand');
+
+    // this.tick();
+  }
+
+  setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsInDegrees = (seconds / 60) * 360;
+
+    this.secondHand.attributes.style.transform = `rotate(${seconds}deg)`; // webkittransform??? 
+  }
+
+  tick() {
+    setInterval(this.setDate, 1000);
+  }
 }
 
 const clock = new Clock();
+
+
+
+// base 100 the time
